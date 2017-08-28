f678a26f9f3aac2f84e7b4bd72b498909793b640
Gabe Johnson <gijohnson105@gmail.com>
2017-08-28 11:04:38 -0500

    Add STORAGE Effect

--- src/Web/Storage.purs
+++ src/Web/Storage.purs
@@ -10,11 +10,9 @@ module Web.Storage
 
 import Prelude
 
+import Web.Storage.Types (STORAGE, Storage)
+
 import Control.Monad.Eff (Eff)
-
-import DOM (DOM)
-import Web.Storage.Types (Storage)
-
 import Data.Maybe (Maybe)
 import Data.Nullable (Nullable, toMaybe)
 
@@ -22,16 +20,20 @@ foreign import length :: forall eff. Storage -> Eff (dom :: DOM | eff) Int
 
 foreign import _key :: forall eff. Int -> Storage -> Eff (dom :: DOM | eff) (Nullable String)
 
-key :: forall eff. Int -> Storage -> Eff (dom :: DOM | eff) (Maybe String)
+foreign import length :: forall eff. Storage -> Eff (storage :: STORAGE | eff) Int
+
+foreign import _key :: forall eff. Int -> Storage -> Eff (storage :: STORAGE | eff) (Nullable String)
+
+key :: forall eff. Int -> Storage -> Eff (storage :: STORAGE | eff) (Maybe String)
 key i = map toMaybe <<< _key i
 
-foreign import _getItem :: forall eff. String -> Storage -> Eff (dom :: DOM | eff) (Nullable String)
+foreign import _getItem :: forall eff. String -> Storage -> Eff (storage :: STORAGE | eff) (Nullable String)
 
-getItem :: forall eff. String -> Storage -> Eff (dom :: DOM | eff) (Maybe String)
+getItem :: forall eff. String -> Storage -> Eff (storage :: STORAGE | eff) (Maybe String)
 getItem s = map toMaybe <<< _getItem s
 
-foreign import setItem :: forall eff. String -> String -> Storage -> Eff (dom :: DOM | eff) Unit
+foreign import setItem :: forall eff. String -> String -> Storage -> Eff (storage :: STORAGE | eff) Unit
 
-foreign import removeItem :: forall eff. String -> Storage -> Eff (dom :: DOM | eff) Unit
+foreign import removeItem :: forall eff. String -> Storage -> Eff (storage :: STORAGE | eff) Unit
 
-foreign import clear :: forall eff. Storage -> Eff (dom :: DOM | eff) Unit
+foreign import clear :: forall eff. Storage -> Eff (storage :: STORAGE | eff) Unit
--- src/Web/Storage/Types.purs
+++ src/Web/Storage/Types.purs
@@ -1,8 +1,11 @@
 module Web.Storage.Types where
 
+import Control.Monad.Eff (kind Effect)
 import Data.Foreign (Foreign, F, unsafeReadTagged)
 
 foreign import data Storage :: Type
+
+foreign import data STORAGE :: Effect
 
 readStorage :: Foreign -> F Storage
 readStorage = unsafeReadTagged "Storage"
